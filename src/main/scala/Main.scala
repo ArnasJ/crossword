7135096e71f55b1815ff8615908293e95e63ea1d
Arnas Janulionis <arnas.janulionis@gmail.com>
2018-06-20 18:00:59 +0300

    WIP: crossword

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,20 +2,47 @@ import scala.io.Source
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val file = Source.fromFile("input.txt").getLines.toList
-    val givenWords = file.head.split(" ")
-    val crosswordLines = file.tail.filter(_.nonEmpty)
-    val numberOfRows = crosswordLines.map(_.length).max
-    val numberOfColumns = crosswordLines.length
+    val file = Source.fromFile("input.txt").getLines.toVector
+    val givenWords = file.head.split(" ").toVector
+    val unparsedLines = file.tail.filter(_.nonEmpty)
+    val parsedLines = unparsedLines.map(line =>
+      line.zipWithIndex.map(c => Cell(c._2, unparsedLines.indexOf(line), tryGetLetter(c._1)))
+    )
+    val crossword = parsedLines.flatten
+    val wordsToSolve = findWords(crossword)
+
+    val possibleSolutions = solve(crossword, wordsToSolve, givenWords)
+
+    possibleSolutions match {
+      case None => println("No possible solutions.")
+      case Some(solutions) => solutions.foreach(s => ???)
+    }
   }
 
   sealed trait Letter
   case class Vowel(value: Option[Char]) extends Letter
   case class Consonant(value: Option[Char]) extends Letter
 
-  case class Cell(xCoord: Int, yCoord: Int, isFirst: Boolean, letter: Letter)
+  case class Cell(xCoord: Int, yCoord: Int, letter: Option[Letter])
 
-  case class Word(cells: List[Cell])
+  case class Word(cells: Vector[Cell])
 
-  case class Crossword(words: List[Word])
+  def tryGetLetter(c: Char) : Option[Letter] =
+    c match {
+      case 'X' => Some(Consonant(None))
+      case 'O' => Some(Vowel(None))
+      case _ => None
+    }
+
+  def findWords(crossword: Vector[Cell]): Vector[Word] = {
+    def go(crw: Vector[Cell], res: Vector[Word]): Vector[Word] = ???
+
+    go(crossword, Vector.empty)
+  }
+
+  def solve(
+    crossword: Vector[Cell],
+    wordsToSolve: Vector[Word],
+    givenWords: Vector[String]
+  ) : Option[Vector[Vector[Cell]]] = ???
 }
